f4550746af67db88e3c7a8a180473a4865b6b107
Praveen-Cyber311 <nagapraveen311@gmail.com>
2025-02-19 20:39:35 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+from engine import Encoder, Decoder
+
+def main():
+    e = Encoder()
+    d = Decoder()
+
+    img, name = e.encode("this is some text.", save=True)
+    img.show()
+    d.decode(name, show=True)
+
+if __name__ == "__main__":
+    main()
